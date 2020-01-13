6d833b78ba05f2f08c5ea4c9dd4fd19742dead93
Your Name <youremail@domain.com>
2020-01-13 14:02:59 -0700

    Updates

---
+++ precache-manifest.be3c6402272fefa748d3babb7b50fa44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64dda80c11b1d7a8413ac8730702029b",
+    "url": "/test2-extractorxml/index.html"
+  },
+  {
+    "revision": "dd58399ebf1132f79dbd",
+    "url": "/test2-extractorxml/static/css/2.66e62f61.chunk.css"
+  },
+  {
+    "revision": "5d391dd56bc0f811951e",
+    "url": "/test2-extractorxml/static/css/main.60b39835.chunk.css"
+  },
+  {
+    "revision": "dd58399ebf1132f79dbd",
+    "url": "/test2-extractorxml/static/js/2.0805f94e.chunk.js"
+  },
+  {
+    "revision": "8bf4122c1c935c11d3e585bc7556b767",
+    "url": "/test2-extractorxml/static/js/2.0805f94e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d391dd56bc0f811951e",
+    "url": "/test2-extractorxml/static/js/main.eb6ffcec.chunk.js"
+  },
+  {
+    "revision": "4d435d1743ed660258f4",
+    "url": "/test2-extractorxml/static/js/runtime-main.15343b5b.js"
+  },
+  {
+    "revision": "91efa191648811866106935f4c1c38dd",
+    "url": "/test2-extractorxml/static/media/PHOTO-PAGE-ACCUEIL.91efa191.jpg"
+  }
+]);
